76ab1b19e12ee4131b1e4574d29c566cf288fa08
Sherry <593401261@qq.com>
2015-02-11 14:06:31 +0800

    change

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,9 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World"); 
+		//push test test
 	}
 
 }
