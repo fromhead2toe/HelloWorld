d4018cd19ccb3b0ad4eae242b5d571f3de7a36eb
Sherry <xsunaf@stu.ust.hk>
2015-02-25 14:52:26 +0800

    ppp

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,48 @@
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World"); 
-		//push test test
-	}
+	
+		static int cal(String s){
+			String[] temp = s.split(" ");
+			int num1=Integer.parseInt(temp[0]);
+			int num2=Integer.parseInt(temp[2]);
 
-}
+			
+			switch(temp[1]){
+			case"+": return num1+num2;
+			case"-": return num1-num2;
+			}
+			
+			return 0;
+
+	}
+	public static void main(String[] args) {
+		//System.out.println("Hello World"); 
+		//push test test
+		//String a = "bpple";
+		//String b = "banana";
+		//System.out.println(a.compareTo(b));
+		//System.out.println(a.charAt(4));
+		
+		
+	//String i = "3";
+	//int q =3;
+	
+	//int temp =Integer.parseInt(i);
+	/*	int lenth = args.length();
+		for(int i=0;i<lenth;i++){
+			char ch = args.charAt(i);
+			if(Character.isUpperCase(ch)){
+				System.out.println(ch);
+				args.
+			}
+		}*/
+		
+	System.out.println(cal("2 - 3"));
+	
+	
+	}
+}	
+
+
+
---
+++ HelloWorld/src/commit_test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sherry
+ *
+ */
+public class commit_test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
